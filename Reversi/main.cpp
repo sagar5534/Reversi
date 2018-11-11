46b86b385cdf201bd7992b3a886d70888acd2edc
Sagar Patel <sagar.rpatel@outlook.com>
2018-11-11 16:22:40 -0500

    Initial Commit

---
+++ Reversi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Reversi
+//
+//  Created by Luna on 2018-11-11.
+//  Copyright © 2018 Sagar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
