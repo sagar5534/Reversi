cac5adae6167fb7decd9e9fe8a2b9e7711da8d6b
Sagar Patel <sagar.rpatel@outlook.com>
2018-11-11 19:36:37 -0500

    Back to fresh

--- Reversi/main.cpp
+++ Reversi/main.cpp
@@ -12,13 +12,6 @@ using namespace std;
 int main() {
     // insert code here...
     cout << "Hello, World!\n";
-    cout<<"Whats up!"<<endl;
-    cout << "What a DICK!!";
-<<<<<<< HEAD
-    cout<<"Noob"<<endl; 
-=======
-    cout << "nil is a noob!!";
->>>>>>> f1dbaf53169b67c2961c04bae0dd580f294cfb37
-    return 0;
 
+     
 }
