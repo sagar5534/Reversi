53e1b32fc1fc3676ed4f0e78442d0d9f992199fe
esfar2 <35281270+esfar2@users.noreply.github.com>
2018-11-12 18:18:10 -0500

    Merge pull request #1 from sagar5534/ComputerPlayer
    Computer player

--- Reversi/Game.hpp
+++ Reversi/Game.hpp
@@ -10,5 +10,20 @@
 #define Game_hpp
 
 #include <stdio.h>
+#include "Board.hpp"
+#include "Player.hpp"
 
+class Game{
+private:
+    Board board;
+    Player *p1,*p2;
+public:
+    Game();
+    ~Game();
+    //void selectPlayers();
+    //Player* nextPlayer() const;
+    //void play();
+    //void announceWinner();
+    
+};
 #endif /* Game_hpp */
