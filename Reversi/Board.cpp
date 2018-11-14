ff744d7b276d209d6b8d0aa284391f3d2bef094f
Sagar Patel <sagar.rpatel@outlook.com>
2018-11-14 01:05:43 -0500

    Created Functions

--- Reversi/Board.cpp
+++ Reversi/Board.cpp
@@ -9,5 +9,46 @@
 #include "Board.hpp"
 
 Board::Board(){
-    
+
+}
+
+void Board::reset(){
+
+}
+
+void Board::display(){
+
+}
+
+void Board::makeMove(Piece piece, Move move){
+
+}
+
+bool Board::isLegal(Piece piece, Move move) const {
+
+  return 0;
+}
+
+Piece Board::getWinner() const{
+
+  return Piece::WHITE;
+}
+
+Piece Board::getPlayer() const{
+
+  return Piece::WHITE;
+}
+
+void Board::getMoves(){
+
+}
+
+int Board::numMoves() const{
+
+  return 0;
+}
+
+Move Board::getMove(int move) const{
+
+  return 0;
 }
