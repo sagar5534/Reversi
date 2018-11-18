d971e683769f469254b7673da559e5e21637d647
esfar2 <35281270+esfar2@users.noreply.github.com>
2018-11-18 05:56:31 -0500

    Update Reversi/ComputerPlayer.hpp

--- Reversi/ComputerPlayer.hpp
+++ Reversi/ComputerPlayer.hpp
@@ -9,20 +9,22 @@
 #ifndef ComputerPlayer_hpp
 #define ComputerPlayer_hpp
 
-#include <stdio.h>
+
 #include "Player.hpp"
+#include <iostream>
 
 class ComputerPlayer : public Player{
 
 private:
-    string name;
-    Piece piece;
+
     static int numberOfInstance;
 
 public:
-    ComputerPlayer(Piece);
-    virtual ~ComputerPlayer();
-    //Piece getPiece() const;
+
+    ComputerPlayer(Piece i );
+    ~ComputerPlayer();
+    
+
 };
 
 #endif /* ComputerPlayer_hpp */
